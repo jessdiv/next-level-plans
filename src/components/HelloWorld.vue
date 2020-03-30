0fb340dcb7c83482fb48c7e34e36fb47b0efaa7d
jessdiv <jessica.diver@hotmail.com>
2020-03-30 21:27:22 +1100

    logo and services structure in

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,151 +1,88 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+    <v-container>
+        <v-row class="text-center">
+            <v-img src="../assets/images/EMAILBANNERNICE.jpg"></v-img>
+            <v-img src="../assets/images/photo-1503387837-b154d5074bd2.jpeg"></v-img>
+            <section id="features">
+                <v-container class="text-center">
+                    <h2 class="display-2 font-weight-bold mb-3">Services</h2>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
+                    <v-responsive class="mx-auto mb-12" width="56">
+                        <v-divider class="mb-1"></v-divider>
+                    </v-responsive>
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
+                    <v-row>
+                        <v-col
+                            v-for="({ icon, title, text }, i) in features"
+                            :key="i"
+                            cols="12"
+                            md="4"
+                        >
+                            <v-card class="py-12 px-4" color="grey lighten-5" flat>
+                                <v-theme-provider dark>
+                                    <div>
+                                        <v-avatar color="#FFDD00" size="88">
+                                            <v-icon large v-text="icon"></v-icon>
+                                        </v-avatar>
+                                    </div>
+                                </v-theme-provider>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+                                <v-card-title
+                                    class="justify-center font-weight-black text-uppercase"
+                                    v-text="title"
+                                ></v-card-title>
 
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
+                                <v-card-text class="subtitle-1" v-text="text"></v-card-text>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-container>
+
+                <div class="py-12"></div>
+            </section>
         </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+    </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+export default {
+    name: "HelloWorld",
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
-  }
+        features: [
+            {
+                icon: "mdi-pencil-ruler",
+                title: "Measure & Sketch",
+                text:
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam"
+            },
+            {
+                icon: "mdi-pencil",
+                title: "Drafting",
+                text:
+                    "Sed ut elementum justo. Suspendisse non justo enim. Vestibulum cursus mauris dui, a luctus ex blandit. Lorem ipsum dolor sit amet consectetur adipisicing elit. qui ipsum eveniet facilis obcaecati corrupti consectetur adipisicing elit."
+            },
+            {
+                icon: "mdi-camera",
+                title: "Drone Photography",
+                text:
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam"
+            }
+        ],
+        items: [
+            {
+                src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+            },
+            {
+                src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg"
+            },
+            {
+                src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+            },
+            {
+                src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+            }
+        ]
+    })
+};
 </script>
